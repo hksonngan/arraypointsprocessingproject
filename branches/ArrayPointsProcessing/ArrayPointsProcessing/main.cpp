413919b58d5e80e5e5619f08a9d8773150117b84
Lebedev.Ilya.G@gmail.com <Lebedev.Ilya.G@gmail.com@9f64be17-c248-32b9-2fe5-dc10fbaacc9c>
2010-12-09 12:53:31 +0000

    Исправлена ошибка в структурах данных

--- branches/ArrayPointsProcessing/ArrayPointsProcessing/main.cpp
+++ branches/ArrayPointsProcessing/ArrayPointsProcessing/main.cpp
@@ -3,40 +3,9 @@
 
 using namespace std;
 
-void LoadArray(PointArray& pointArray, const char* nameFile)
-{
-    std::ifstream fs(nameFile,std::ios::in | std::ios::binary);
-
-    fs.read((char*)&pointArray.sizeX,sizeof(int));
-    fs.read((char*)&pointArray.sizeY,sizeof(int));
-    fs.read((char*)&pointArray.sizeZ,sizeof(int));
-
-    fs.read((char*)&pointArray.scaleX,sizeof(float));
-    fs.read((char*)&pointArray.scaleX,sizeof(float));
-    fs.read((char*)&pointArray.scaleX,sizeof(float));
-
-    pointArray.dataX = new short[pointArray.sizeX];
-    pointArray.dataY = new short[pointArray.sizeY];
-    pointArray.dataZ = new short[pointArray.sizeZ];
-
-    fs.read((char*)pointArray.dataX,2*pointArray.sizeX);
-    fs.read((char*)pointArray.dataY,2*pointArray.sizeY);
-    fs.read((char*)pointArray.dataZ,2*pointArray.sizeZ);
-
-    fs.close();
-}
-
-void DestroyArray(PointArray& pointArray)
-{
-    delete [] pointArray.dataX;
-    delete [] pointArray.dataY;
-    delete [] pointArray.dataZ;
-}
-
-
 int main(int argc, char *argv[])
 {
     PointArray pointArray;
-    LoadArray(pointArray, "brain_t1.bin");
+    LoadArray(pointArray, "brain_t2.bin");
     DestroyArray(pointArray);
 }
