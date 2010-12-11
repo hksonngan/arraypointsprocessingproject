a448c52dd4064357b22ba0c0dc34eada821d3513
qqwweerrttyy8 <qqwweerrttyy8@gmail.com>
2010-12-11 06:31:45 +0000

    Exclude main.cpp from project Added PointArrayStructure.cpp Set project configuration from .exe to .lib Added namespace token in both PointArrayStructure.*

--- ArrayPointsProcessing/PointArrayStructure.h
+++ ArrayPointsProcessing/PointArrayStructure.h
@@ -1,70 +1,46 @@
 #ifndef __PointArrayStructure_h__
 #define __PointArrayStructure_h__
 
-//все исходные данные
-struct PointArray 
+#include <fstream>
+
+namespace ArrayPointsProcessing
 {
-    short* data; //Плотность в точке
-    int sizeX; //размер по х
-    int sizeY;
-    int sizeZ;
-    int scaleX;
-    int scaleY;
-    int scaleZ;
-};
 
-//слой плотностей (x*y плоскость), размер слоя - sizeX * sizeY
-struct Layer
-{
-    Layer(): data(NULL), sizeX(0), sizeY(0) {}
-    Layer(short* density, int x, int y): data(density), sizeX(x), sizeY(y) {}
-    short* data; //указатель на начало слоя
-    int sizeX; //размер по х
-    int sizeY; //размер по х
-};
+	//все исходные данные
+	struct PointArray 
+	{
+		short* data; //Плотность в точке
+		int sizeX; //размер по х
+		int sizeY;
+		int sizeZ;
+		int scaleX;
+		int scaleY;
+		int scaleZ;
+	};
 
-//загрузка данных из файла nameFile
-void LoadArray(PointArray& pointArray, const char* nameFile)
-{
-    std::ifstream fs(nameFile,std::ios::in | std::ios::binary);
+	//слой плотностей (x*y плоскость), размер слоя - sizeX * sizeY
+	struct Layer
+	{
+		Layer(): data(NULL), sizeX(0), sizeY(0) {}
+		Layer(short* density, int x, int y): data(density), sizeX(x), sizeY(y) {}
+		short* data; //указатель на начало слоя
+		int sizeX; //размер по х
+		int sizeY; //размер по х
+	};
 
-    fs.read((char*)&pointArray.sizeX,sizeof(int));
-    fs.read((char*)&pointArray.sizeY,sizeof(int));
-    fs.read((char*)&pointArray.sizeZ,sizeof(int));
+	//загрузка данных из файла nameFile
+	void LoadArray(PointArray& pointArray, const char* nameFile);
 
-    fs.read((char*)&pointArray.scaleX,sizeof(float));
-    fs.read((char*)&pointArray.scaleY,sizeof(float));
-    fs.read((char*)&pointArray.scaleZ,sizeof(float));
+	//уничтожение исходных данных
+	void DestroyArray(PointArray& pointArray);
 
-    pointArray.data = new short[pointArray.sizeX * pointArray.sizeY * pointArray.sizeZ];
-    fs.read((char*)pointArray.data,2*pointArray.sizeX * pointArray.sizeY * pointArray.sizeX);
+	//возвращает слой с координатой z(измняется от 0 до sizeZ)
+	Layer GetLayer(PointArray& pointArray, int z);
 
+	//возвращает плотность в точке слоя с координатами  x(измняется от 0 до sizeX), y(измняется от 0 до sizeY)
+	short GetDensity(Layer& layer, int x, int y);
 
-    fs.close();
+	//возвращает плотность в точке пространства с координатами  x(измняется от 0 до sizeX), y(измняется от 0 до sizeY), z(измняется от 0 до sizeZ)
+	short GetDensity(PointArray& pointArray, int x, int y, int z);
 }
-
-//уничтожение исходных данных
-void DestroyArray(PointArray& pointArray)
-{
-    delete [] pointArray.data;
-}
-
-//возвращает слой с координатой z(измняется от 0 до sizeZ)
-Layer GetLayer(PointArray& pointArray, int z)
-{
-    return Layer(&pointArray.data[z * pointArray.sizeX * pointArray.sizeY], pointArray.sizeX, pointArray.sizeY);
-}
-
-//возвращает плотность в точке слоя с координатами  x(измняется от 0 до sizeX), y(измняется от 0 до sizeY)
-short GetDensity(Layer& layer, int x, int y)
-{
-    return layer.data[x + y * layer.sizeY];
-}
-
-//возвращает плотность в точке пространства с координатами  x(измняется от 0 до sizeX), y(измняется от 0 до sizeY), z(измняется от 0 до sizeZ)
-short GetDensity(PointArray& pointArray, int x, int y, int z)
-{
-    return pointArray.data[x + y * pointArray.sizeY + z * pointArray.sizeX * pointArray.sizeY];
-}
-
 #endif
