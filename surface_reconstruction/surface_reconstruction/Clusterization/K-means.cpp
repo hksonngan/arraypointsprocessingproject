acd4a1efed0243447eae24ffcc98362dbf334996
imhojob <imhojob@9f64be17-c248-32b9-2fe5-dc10fbaacc9c>
2011-01-15 12:00:40 +0000

    Реализована сегментация k-средних на OpenCL. В компоненте DataGridView исправлен недочёт с добавлением новой строки при щелчке мышью в CheckBox-е последней строки таблицы. Настроены Anchor-ы основных компонентов (не знаю, как будет смотреться на других машинах). Добавлена возможность выбора алгоритма сегментации (пока работает только OpenCL-версия, последовательная будет восстановлена, когда отлажу иерархию наследования от абстрактного класса). Работаю над условной сегментацией.

--- surface_reconstruction/surface_reconstruction/Clusterization/K-means.cpp
+++ surface_reconstruction/surface_reconstruction/Clusterization/K-means.cpp
@@ -1,11 +1,34 @@
 #include "stdafx.h"
 #include "K-means.h"
 #include <math.h>
-//#include <cl.h>
+#include <cl.h>
 
 using namespace std;
 
+const char * source_distribution = "__kernel void distribution ( __global short	* data,			\n"\
+																"__global float * means,		\n"\
+																"__global unsigned int * labels,\n"\
+																"unsigned int VoxelsCount,		\n"\
+																"unsigned int ClustersCount )	\n"\
 
+"{																	\n"\
+ "unsigned int i = get_global_id (0);								\n"\
+ "if (i<VoxelsCount)												\n"\
+ "{																	\n"\
+  "unsigned int optimal_cluster_id = 0;								\n"\
+  "float min_dist = (float)INT_MAX;									\n"\
+  "for (unsigned int j = 0; j < ClustersCount; ++j) if (means[j]>=0)\n"\
+  "{																\n"\
+   "float new_dist = fabs((float)data[i]-means[j]);					\n"\
+   "if (new_dist < min_dist)										\n"\
+   "{																\n"\
+    "optimal_cluster_id = j;										\n"\
+    "min_dist = new_dist;											\n"\
+   "}																\n"\
+  "}																\n"\
+  "labels[i] = optimal_cluster_id;									\n"\
+ "}																	\n"\
+"}"; 
 
 void KmeansMethod::ReleaseResources()
 {
@@ -30,8 +53,6 @@ KmeansMethod::KmeansMethod(Layer layer)
  DataSize = layer.sizeX*layer.sizeY;
 }
 
-KmeansMethod::KmeansMethod() { ReleaseResources(); }
-
 KmeansMethod::~KmeansMethod() { ReleaseResources(); }
 
 void KmeansMethod::SetClustersNumber(size_t value) { NClusters = value; }
@@ -39,9 +60,9 @@ void KmeansMethod::SetClustersNumber(size_t value) { NClusters = value; }
 void KmeansMethod::SetCountIterations(size_t value) { CountIterations = value; }
 
 // Вычисление центра тяжести для i-ого кластера
-double KmeansMethod::GetMean(size_t i)
+float KmeansMethod::GetMean(size_t i)
 {
- double Sum = 0;
+ float Sum = 0;
  vector <size_t>::iterator iter = Clusters[i].begin();
  vector <size_t>::iterator _end = Clusters[i].end();
  for (; iter!=_end; ++iter) Sum += InputData->data[*iter];
@@ -50,9 +71,11 @@ double KmeansMethod::GetMean(size_t i)
 
 vector <vector <size_t> > KmeansMethod::GetClusters(BackgroundWorker^ worker)
 {
- size_t part = DataSize/NClusters;
-
+ means = new float [NClusters];
+ 
  Clusters = new vector <size_t> [NClusters];
+	
+ size_t part = DataSize/NClusters;
 
  for (size_t i = 0; i < NClusters; ++i)
  {
@@ -60,7 +83,7 @@ vector <vector <size_t> > KmeansMethod::GetClusters(BackgroundWorker^ worker)
   Clusters[i].push_back(j);
  }
 
- means = new double [NClusters];
+ // printf("Clusters are initialized!"); _getch();
 
  for (size_t iter = 1; iter <= CountIterations; ++iter)
  {
@@ -70,17 +93,17 @@ vector <vector <size_t> > KmeansMethod::GetClusters(BackgroundWorker^ worker)
    means[i] = GetMean(i); 
    Clusters[i].clear();
   }
-  else means[i] = -1;
-	
+  else means[i] = -1.0f;
+
   for (size_t j = 0; j < DataSize; ++j)
   {
    size_t optimal_cluster_id = 0;
 
-   double min_dist = (double)INT_MAX;
+   float min_dist = (float)INT_MAX;
 
    for (size_t k = 0; k < NClusters; ++k) if (means[k]>=0)
    {
-    double new_dist = fabs((double)InputData->data[j]-means[k]);
+    float new_dist = fabs((float)InputData->data[j]-means[k]);
     if (new_dist < min_dist)
     {
      optimal_cluster_id = k;
@@ -89,7 +112,7 @@ vector <vector <size_t> > KmeansMethod::GetClusters(BackgroundWorker^ worker)
    }
 
    Clusters[optimal_cluster_id].push_back(j);
-  }
+  } 
 
   worker->ReportProgress(100*iter/CountIterations);
  }
@@ -100,4 +123,254 @@ vector <vector <size_t> > KmeansMethod::GetClusters(BackgroundWorker^ worker)
  if (Clusters[i].size()>0) result.push_back(Clusters[i]);
 
  return result;
+}
+
+vector <vector <size_t> > KmeansOpenCLMethod::GetClusters(BackgroundWorker^ worker)
+{
+  // Инициализация контекста
+  cl_uint numPlatforms = 0;
+
+  clGetPlatformIDs(0,            /* number of entries */
+				   NULL,         /* платформы */
+				   &numPlatforms /* число платформ */);
+
+  cl_platform_id platform = NULL;
+
+  if (numPlatforms > 0)
+  {
+   cl_platform_id * platforms = new cl_platform_id [numPlatforms];
+
+   clGetPlatformIDs(numPlatforms, 
+    	            platforms, 
+				    NULL);
+
+   platform = platforms[0];
+
+   delete [] platforms;
+  }
+
+  // Создаём свойства контекста для заданной платформы 
+  cl_context_properties properties [3] = { CL_CONTEXT_PLATFORM, (cl_context_properties) platform, 0 };
+
+  cl_context context = clCreateContextFromType((NULL == platform) ? NULL : properties, 
+											    CL_DEVICE_TYPE_GPU, 
+											    NULL, 
+											    NULL, 
+											    NULL);
+
+  size_t Size = 0;
+
+  clGetContextInfo(context, CL_CONTEXT_DEVICES, 0, NULL, &Size);
+
+  cl_device_id device = NULL;
+
+  if (Size > 0)
+  {
+   cl_device_id * devices = ( cl_device_id * ) alloca ( Size );
+
+   clGetContextInfo (context			/* context */,
+                     CL_CONTEXT_DEVICES /* param_name */,
+  					 Size				/* param_value_size */,
+				     devices			/* param_value */,
+                     NULL				/* param_value__size_ret */ );
+
+   device = devices [0];
+  }
+
+  // Создаем очередь команд для заданного контекста и выбранного устройства
+  cl_command_queue queue = clCreateCommandQueue (context /* context */,
+												 device	 /* device */,
+												 0		 /* properties */,
+												 NULL	 /* errcode_ret */ );
+
+ // Создаем программный объект из исходного кода (определен выше)
+  size_t srclen [] = { strlen ( source_distribution ) };
+
+  cl_program program = clCreateProgramWithSource (context						/* context */,
+												  1								/* count */	 ,
+												  &source_distribution			/* strings */,
+												  srclen						/* lengths */,
+												  NULL							/* errcode_ret */ );
+
+  // Создаем исполняемый файл программы для выбранного устройства (GPU)
+  clBuildProgram (program  /* program */,
+				  1		   /* num_devices */,
+				  &device  /* device_list */,
+				  NULL	   /* options */,
+				  NULL     /* pfn_notify */,
+                  NULL     /* user_data */ );
+
+
+  // Создаем объект ядра для перераспределения вокселов между кластерами
+  cl_kernel kernel = clCreateKernel ( program		/* program */,
+									 "distribution"	/* kernel_name */,
+									  NULL			/* errcode_ret */ );
+
+  // Создаем объект памяти в виде буфера для передачи ядру входного массива
+  cl_mem input_data = clCreateBuffer (context					  /* context */,
+									  CL_MEM_READ_ONLY		      /* flags */,
+								      sizeof (short) * DataSize /* size */,
+								      NULL					      /* host_ptr */,
+								      NULL					      /* errcode_ret */ );
+  
+  cl_mem input_means = clCreateBuffer (context, 
+									   CL_MEM_READ_ONLY, 
+									   sizeof (float) * NClusters, 
+									   NULL, 
+									   NULL);	// буфер для передачи центров тяжести
+
+  cl_mem output_labels = clCreateBuffer (context, 
+										 CL_MEM_WRITE_ONLY, 
+										 sizeof (unsigned int) * DataSize, 
+										 NULL, 
+										 NULL); // буфер для передачи меток кластеров
+ 
+ // Задание аргументов ядра
+  clSetKernelArg (kernel			/* kernel		*/,
+				  0					/* arg_index	*/,
+				  sizeof ( cl_mem )	/* arg_size		*/,
+				  &input_data		/* arg_value	*/ );
+ 
+  clSetKernelArg (kernel			/* kernel		*/,
+				  1					/* arg_index	*/,
+				  sizeof ( cl_mem )	/* arg_size		*/,
+				  &input_means		/* arg_value	*/ );
+
+  clSetKernelArg (kernel			/* kernel		*/,
+				  2					/* arg_index	*/,
+				  sizeof ( cl_mem ) /* arg_size		*/,
+				  &output_labels	/* arg_value	*/ );
+
+  clSetKernelArg (kernel, 3, sizeof (unsigned int), &DataSize);
+
+  clSetKernelArg (kernel, 4, sizeof (unsigned int), &NClusters);
+
+  size_t group; // максимальный размер группы работ
+
+  // автоматическое определение максимального размера группы работ
+  clGetKernelWorkGroupInfo (kernel						/* kernel */,
+						    device						/* device */,
+						    CL_KERNEL_WORK_GROUP_SIZE	/* param_name */,
+						    sizeof ( size_t )			/* param_value_size */,
+						    &group						/* param_value */,
+						    NULL						/* param_value_size_ret */ );
+
+ size_t part = DataSize/NClusters;
+
+ means = new float [NClusters];
+
+ Labels = new size_t [DataSize];
+
+ Clusters = new vector <size_t> [NClusters];
+
+ for (size_t i = 0; i < NClusters; ++i)
+ {
+  for (size_t j = part*i; j < max(DataSize*(i/(NClusters-1)), part*(i+1)); ++j)
+  Clusters[i].push_back(j);
+ }
+	
+ for (size_t iter = 1; iter <= CountIterations; ++iter)
+ {
+  // Вычисление центров тяжести кластеров
+  for (size_t i = 0; i < NClusters; ++i)
+  if (Clusters[i].size()>0) 
+  {
+   means[i] = GetMean(i); 
+   Clusters[i].clear();
+  }  
+  else means[i] = -1.0f;
+
+  // Помещаем в очередь команду записи входного массива в объект памяти
+  clEnqueueWriteBuffer (queue						/* command_queue */,
+					    input_data					/* buffer */,
+					    CL_TRUE						/* blocking_write */,
+					    0							/* offset */,
+					    sizeof ( short ) * DataSize	/* cb */,
+						InputData->data				/* ptr */,
+					    0							/* num_events_in_wait_list */,
+					    NULL						/* event_wait_list */,
+					    NULL						/* event */ );
+
+  clEnqueueWriteBuffer (queue, output_labels, CL_TRUE, 0, sizeof (unsigned int) * DataSize, Labels, 0, NULL, NULL);
+ 
+  clEnqueueWriteBuffer (queue, input_means, CL_TRUE, 0, sizeof (float) * NClusters, means, 0, NULL, NULL);
+
+  // Выполнение ядра над всем множеством входных данных
+  clEnqueueNDRangeKernel (queue		/* command_queue */,
+   						  kernel	/* kernel */,
+						  1			/* work_dim */,
+						  NULL		/* global_work_offset */,
+						  &DataSize	/* global_work_size */,
+						  &group	/* local_work_size */,
+						  0			/* num_events_in_wait_list */,
+						  NULL		/* event_wait_list */,
+						  NULL		/* event */ );
+
+  clFinish ( queue ); // Ожидаем завершения всех команд в очереди
+
+  // Загрузка результатов вычислений с устройства
+  clEnqueueReadBuffer (queue								 /* command_queue */,
+					   output_labels						 /* buffer */,
+					   CL_TRUE								 /* blocking_read */,
+					   0									 /* offset */,
+					   sizeof (unsigned int) * DataSize		 /* cb */,
+					   Labels								 /* ptr */,
+					   0									 /* num_events_in_wait_list */,
+					   NULL									 /* event_wait_list */,
+					   NULL									 /* event */ );
+
+  // формирование новых кластеров
+  for (size_t i = 0; i < DataSize; ++i) Clusters[Labels[i]].push_back(i);
+
+  worker->ReportProgress(100*iter/CountIterations);
+ }
+ 
+ // printf("\n");
+
+ // освобождение занятых ресурсов 
+ clReleaseMemObject ( input_data );
+ clReleaseMemObject ( input_means );
+ clReleaseMemObject ( output_labels );
+ clReleaseProgram ( program );
+ clReleaseKernel ( kernel );
+ clReleaseCommandQueue ( queue );
+ clReleaseContext ( context );
+
+ vector <vector <size_t> > result; result.clear();
+ for (size_t i = 0; i < NClusters; ++i)
+ if (Clusters[i].size()>0) result.push_back(Clusters[i]);
+
+ return result;
+}
+
+// Класс KmeansConditionalMethod
+
+size_t KmeansConditionalMethod::VoxelsNumberInRange()
+{
+ size_t result = 0;
+ for (size_t i = 0; i < DataSize; ++i)
+ if ((InputData->data[i]>=MinDensityRange)&&
+	 (InputData->data[i]<=MaxDensityRange))
+	 result++;
+ return result;
+}
+
+float KmeansConditionalMethod::GetMean(size_t i)
+{
+ float Sum = 0;
+ for (size_t i = 0; i < DataSize; ++i)
+ if ((InputData->data[i]>=MinDensityRange)&&
+	 (InputData->data[i]<=MaxDensityRange))
+ Sum+=InputData->data[i];
+ return Sum/Clusters[i].size();
+}
+
+vector <vector <size_t> > KmeansConditionalMethod::GetClusters(BackgroundWorker^ worker)
+{
+	// Основной код - будет написан позже
+	
+	vector <vector <size_t> > result; result.clear();
+	for (size_t i = 0; i < NClusters; ++i) 
+		if (Clusters[i].size()>0) result.push_back(Clusters[i]);
+	return result;
 }
--- surface_reconstruction/surface_reconstruction/Clusterization/K-means.h
+++ surface_reconstruction/surface_reconstruction/Clusterization/K-means.h
@@ -5,25 +5,51 @@ using namespace System::ComponentModel;
 
 class KmeansMethod
 {
- private:
-	     size_t NClusters;						// количество кластеров
-		 double* means;							// центры тяжести кластеров
-		 size_t CountIterations;				// количество итераций метода
-		 size_t DataSize;						// размер данных для кластеризации
-		 ScanData* InputData;					// данные для кластеризации
-		 std::vector <size_t>* Clusters;		// кластеры, между которыми распределяются данные 
-		 double GetMean(size_t i);				// вычисление центра тяжести для i-ого кластера
-		 void ReleaseResources();				// освобождение занимаемых ресурсов
-		
- public:
-		
+ protected:
+			size_t NClusters;						// количество кластеров
+			float* means;							// центры тяжести кластеров
+			size_t CountIterations;					// количество итераций метода
+			size_t DataSize;						// размер данных для кластеризации
+			ScanData* InputData;					// данные для кластеризации
+			std::vector <size_t>* Clusters;			// кластеры, между которыми распределяются данные 
+			float GetMean(size_t i);				// вычисление медианы для i-ого кластера
+			void ReleaseResources();				// освобождение занимаемых ресурсов
 
+ public:
 	    // Конструкторы и деструктор
-		KmeansMethod(ScanData* DataValue);
+	    KmeansMethod(ScanData* DataValue);
 		KmeansMethod(Layer layer);
-		KmeansMethod();
 		~KmeansMethod();
-		void SetClustersNumber(size_t value);	// установить число кластеров
-		void SetCountIterations(size_t value);	// установить число итераций
-		std::vector <std::vector <size_t> > GetClusters( BackgroundWorker^ worker);		// кластеризация методом медиан
+		
+		void SetClustersNumber(size_t value);											// установить число кластеров
+		void SetCountIterations(size_t value);											// установить число итераций
+		std::vector <std::vector <size_t> > GetClusters(BackgroundWorker^ worker);		// кластеризация методом k-средних
 };
+
+class KmeansOpenCLMethod : public KmeansMethod
+{
+ private: size_t* Labels;										// метки кластеров 
+
+ public:
+	 // Конструкторы 
+	 KmeansOpenCLMethod(ScanData* DataValue) : KmeansMethod(DataValue) {};
+	 KmeansOpenCLMethod(Layer layer) : KmeansMethod(layer) {};
+	
+	 std::vector <std::vector <size_t> > GetClusters(BackgroundWorker^ worker);	//кластеризация методом k-средних с использованием OpenCL
+};
+
+class KmeansConditionalMethod: public KmeansMethod			// кластеризация методом k-средних в заданном диапазоне
+{
+ private:
+	short MinDensityRange, MaxDensityRange;					// границы диапазона
+	size_t VoxelsNumberInRange();							// подсчёт числа вокселов в заданном диапазоне
+	float GetMean(size_t i);								// вычисление значения плотности 
+
+ public:
+	 // Конструкторы
+	 KmeansConditionalMethod(ScanData* DataValue): KmeansMethod(DataValue) {};
+	 KmeansConditionalMethod(Layer layer): KmeansMethod(layer) {};
+
+	void SetRangesOfDensity(short left, short right);							// установить диапазон значений плотности
+	std::vector <std::vector <size_t> > GetClusters(BackgroundWorker^ worker);	// условная кластеризация методом k-средних 
+}; 
